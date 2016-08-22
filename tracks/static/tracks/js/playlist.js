f230014818c03d93d86ba7adfda068d19562951b
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2016-08-22 21:47:14 +0200

    Fix playlist.js so that it is error-proof

--- tracks/static/tracks/js/playlist.js
+++ tracks/static/tracks/js/playlist.js
@@ -1,53 +1,55 @@
 $(document).ready(function() {
-    $("#playlist tr").on("click", function() {
-        //switch track
-        $("#player").attr({
-            "src": $(this).attr("data-audiourl"),
-            "autoplay": "autoplay",
-            "preload": "auto"
+    if($("#player")[0]){
+        $("#playlist tr").on("click", function() {
+            //switch track
+            $("#player").attr({
+                "src": $(this).attr("data-audiourl"),
+                "autoplay": "autoplay",
+                "preload": "auto"
+            });
+            //change active row
+            $("#playlist tr.active").removeClass('active');
+            $("#playlist tr.current").removeClass('current');
+            $(this).addClass('active current');
         });
-        //change active row
-        $("#playlist tr.active").removeClass('active');
-        $("#playlist tr.current").removeClass('current');
-        $(this).addClass('active current');
-    });
 
-    //start player with first track
-    $("#player").attr({
-        "src": $("#playlist tr").eq(1).attr("data-audiourl"),
-    });
-    $("#playlist tr").eq(1).addClass('active current');
-
-    document.getElementById('player').onpause = function() {
-        $("#playlist tr.active").removeClass('active');
-    }
-    document.getElementById('player').onplay = function(){
-        $("#playlist tr.current").addClass('active');
-    }
-
-    document.getElementById('player').onended = function(){
+        //start player with first track
         $("#player").attr({
-            "src": '',
+            "src": $("#playlist tr").eq(1).attr("data-audiourl"),
         });
-        var next = $("#playlist tr.current").next()
-        $("#player").attr({
-            "src": next.attr("data-audiourl"),
-        });
-        $('tr.current').removeClass('active current')
-        next.addClass('active current');
-    }
+        $("#playlist tr").eq(1).addClass('active current');
 
-
-    //pause/play on space
-    $(window).keypress(function (e) {
-      if (e.keyCode === 0 || e.keyCode === 32) {
-        e.preventDefault();
-        var player = $("#player")[0];
-        if (player.paused) {
-            player.play();
-        } else {
-            player.pause();
+        $("#player")[0].onpause = function() {
+            $("#playlist tr.active").removeClass('active');
         }
-      }
-    });
+        $("#player")[0].onplay = function(){
+            $("#playlist tr.current").addClass('active');
+        }
+
+        $("#player")[0].onended = function(){
+            $("#player").attr({
+                "src": '',
+            });
+            var next = $("#playlist tr.current").next()
+            $("#player").attr({
+                "src": next.attr("data-audiourl"),
+            });
+            $('tr.current').removeClass('active current')
+            next.addClass('active current');
+        }
+
+
+        //pause/play on space
+        $(window).keypress(function (e) {
+          if (e.keyCode === 0 || e.keyCode === 32) {
+            e.preventDefault();
+            var player = $("#player")[0];
+            if (player.paused) {
+                player.play();
+            } else {
+                player.pause();
+            }
+          }
+        });
+    }
 });
