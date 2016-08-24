e4364396a182834068ad8f4a7dcbe613a8c88bfc
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2016-08-24 22:58:46 +0200

    Add status field to project model

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Event, Project, Track
 
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    list_editable = ('status',)
+    # list_display_links = 'name'
+
+
 # Register your models here.
 admin.site.register(Event)
-admin.site.register(Project)
+# admin.site.register(Project)
 admin.site.register(Track)
+
--- tracks/models.py
+++ tracks/models.py
@@ -2,20 +2,37 @@ from django.db import models
 from datetime import date
 from django.utils.timezone import now
 
+
 class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    date = models.DateField(default=now())
+    date = models.DateField(default=now)
 
     def __str__(self):
         return self.name
 
 
 class Project(models.Model):
+    PROJECT_STATUSES = (
+        ('DRAFT', 'draft'),
+        ('IN PROGRESS', 'in progress'),
+        ('DONE', 'done'),
+        ('OLD', 'old')
+    )
+    PROJECT_STATUSES_CSS = {
+        "DRAFT" : 'label-warning',
+        "IN PROGRESS" : 'label-info',
+        "DONE" : 'label-success',
+        "OLD" : 'label-default',
+    }
+
+    def get_status_css(self):
+        return self.PROJECT_STATUSES_CSS[self.status]
+
     name = models.CharField(max_length=200)
-    date = models.DateField(default=now())
+    date = models.DateField(default=now)
     description = models.TextField(blank=True)
-    # TODO: add state field definig the project's state (done/in progress)
+    status = models.CharField(max_length=15, choices=PROJECT_STATUSES, default='DRAFT')
 
     def __str__(self):
         return self.name
@@ -23,7 +40,7 @@ class Project(models.Model):
 
 class Track(models.Model):
     name = models.CharField(max_length=200)
-    date = models.DateField(now())
+    date = models.DateField(default=now)
     path = models.CharField(max_length=500)
     event = models.ForeignKey(Event)
     project = models.ForeignKey(Project, blank=True, null=True)
